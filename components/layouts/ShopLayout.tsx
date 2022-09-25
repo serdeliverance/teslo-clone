463595f9bb99c90cc67681ef0f0953f303c3bf3d
Sergio Cano <canosergio90@gmail.com>
2022-09-25 12:40:33 +0200

    navbar responsive
    Signed-off-by: Sergio Cano <canosergio90@gmail.com>

--- components/layouts/ShopLayout.tsx
+++ components/layouts/ShopLayout.tsx
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import React, { FC, ReactNode } from 'react'
+import { Navbar } from '../ui'
 
 interface Props {
     title: string
@@ -21,7 +22,7 @@ export const ShopLayout: FC<Props> = ({ children, title, pageDescription, imageF
         </Head>
 
         <nav>
-            {/* Navbar */}
+            <Navbar />
         </nav>
 
         <main style={{
---
+++ components/ui/Navbar.tsx
@@ -0,0 +1,59 @@
+import { AppBar, Badge, Box, Button, IconButton, Link, Toolbar, Typography } from '@mui/material'
+import React from 'react'
+import NextLink from 'next/link'
+import { SearchOutlined, ShoppingCartOutlined } from '@mui/icons-material'
+
+export const Navbar = () => {
+  return (
+    <AppBar>
+        <Toolbar>
+            <NextLink href='/' passHref>
+                <Link display='flex' alignItems='center'>
+                    <Typography variant='h6'>Teslo</Typography>
+                    <Typography sx={{ ml: 0.5 }}>Shop</Typography>
+                </Link>
+            </NextLink>
+
+            <Box flex={1} />
+
+            <Box sx={{ display: { xs: 'none', sm: 'block' }}}>
+                <NextLink href='/category/men' passHref>
+                    <Link>
+                        <Button>Men</Button>
+                    </Link>
+                </NextLink>
+                <NextLink href='/category/women' passHref>
+                    <Link>
+                        <Button>Women</Button>
+                    </Link>
+                </NextLink>
+                <NextLink href='/category/kids' passHref>
+                    <Link>
+                        <Button>Kids</Button>
+                    </Link>
+                </NextLink>
+            </Box>
+
+            <Box flex={1} />
+
+            <IconButton>
+                <SearchOutlined />
+            </IconButton>
+
+            <NextLink href='/cart' passHref>
+                <Link>
+                    <IconButton>
+                        <Badge badgeContent={5} color='secondary'>
+                            <ShoppingCartOutlined />
+                        </Badge>
+                    </IconButton>
+                </Link>
+            </NextLink>
+
+            <Button>
+                Menu
+            </Button>
+        </Toolbar>
+    </AppBar>
+  )
+}
