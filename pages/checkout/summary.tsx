f78fd893caacbe6697f47234f7a268ae666b9396
Sergio Cano <canosergio90@gmail.com>
2022-09-29 22:19:34 +0200

    order summary
    Signed-off-by: Sergio Cano <canosergio90@gmail.com>

--- pages/checkout/summary.tsx
+++ pages/checkout/summary.tsx
@@ -1,7 +1,8 @@
-import { Box, Button, Card, CardContent, Divider, Grid, Typography } from '@mui/material'
+import { Box, Button, Card, CardContent, Divider, Grid, Link, Typography } from '@mui/material'
 import React from 'react'
 import { CartList, OrderSummary } from '../../components/cart'
 import { ShopLayout } from '../../components/layouts'
+import NextLink from 'next/link'
 
 const SummaryPage = () => {
   return (
@@ -10,19 +11,44 @@ const SummaryPage = () => {
 
         <Grid container>
             <Grid item xs={12} sm={7}>
-                <CartList isEditable/>
+                <CartList/>
             </Grid>
             <Grid item xs={12} sm={5}>
                 <Card className='summary-card'>
                     <CardContent>
-                        <Typography variant='h2'>Order</Typography>
+                        <Typography variant='h2'>Summary (3 products)</Typography>
                         <Divider sx={{my: 1}} />
                         
+                        <Box display='flex' justifyContent='space-between'>
+                            <Typography variant='subtitle1'>Delivery address</Typography>
+                            <NextLink href='/checkout/address'>
+                                <Link underline='always'>
+                                    Edit
+                                </Link>
+                            </NextLink>
+                        </Box>
+
+                        <Typography>John Doe</Typography>
+                        <Typography>123 Fake Street</Typography>
+                        <Typography>xxxx</Typography>
+                        <Typography>Canada</Typography>
+                        <Typography>+1 123123</Typography>
+
+                        <Divider sx={{my: 1}} />
+
+                        <Box display='flex' justifyContent='end'>
+                            <NextLink href='/cart'>
+                                <Link underline='always'>
+                                    Edit
+                                </Link>
+                            </NextLink>
+                        </Box>
+
                         <OrderSummary />
 
                         <Box sx={{ mt: 3}}>
                             <Button color='secondary' className='circular-btn' fullWidth>
-                                Checkout
+                                Confirm Order
                             </Button>
                         </Box>
                     </CardContent>
