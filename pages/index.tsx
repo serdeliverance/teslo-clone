0c5f75029c037a0cc43b49988f48277514f61058
Sergio Cano <canosergio90@gmail.com>
2022-10-01 16:45:22 +0200

    adding swr for retrieving data from backend
    Signed-off-by: Sergio Cano <canosergio90@gmail.com>

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,20 +1,28 @@
 import { Typography } from '@mui/material'
 import type { NextPage } from 'next'
+import useSWR from 'swr'
 import { ShopLayout } from '../components/layouts'
 import { ProductList } from '../components/products'
-import { initialData } from '../database/products'
 
-const Home: NextPage = () => {
+const fetcher = (...args: [key: string]) => fetch(...args).then(res => res.json())
+
+const HomePage: NextPage = () => {
+
+  const { data, error } = useSWR('/api/products', fetcher)
+
+  if (error) return <div>failed to load</div>
+  if (!data) return <div>loading...</div>
+
   return (
     <ShopLayout title='Teslo Shop' pageDescription='Find the best products here'>
       <Typography variant='h1' component='h1'>Shop</Typography>
       <Typography variant='h2' sx={{ mb: 1}}>All products</Typography>
 
       <ProductList 
-        products={initialData.products as any}
+        products={data}
       />
     </ShopLayout>
   )
 }
 
-export default Home
+export default HomePage
