dd260e74a6e20e86dd36d12c5ff3dd7edc30a263
Sergio Cano <canosergio90@gmail.com>
2022-10-02 09:00:11 +0200

    adding custom hook for retrieving products and swr global configuration
    Signed-off-by: Sergio Cano <canosergio90@gmail.com>

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,26 +1,24 @@
 import { Typography } from '@mui/material'
 import type { NextPage } from 'next'
-import useSWR from 'swr'
 import { ShopLayout } from '../components/layouts'
 import { ProductList } from '../components/products'
-
-const fetcher = (...args: [key: string]) => fetch(...args).then(res => res.json())
+import { useProducts } from '../hooks/useProducts'
 
 const HomePage: NextPage = () => {
 
-  const { data, error } = useSWR('/api/products', fetcher)
-
-  if (error) return <div>failed to load</div>
-  if (!data) return <div>loading...</div>
+  const { products, isLoading } = useProducts('/products')
 
   return (
     <ShopLayout title='Teslo Shop' pageDescription='Find the best products here'>
       <Typography variant='h1' component='h1'>Shop</Typography>
       <Typography variant='h2' sx={{ mb: 1}}>All products</Typography>
 
-      <ProductList 
-        products={data}
-      />
+      {
+        isLoading
+          ? <h1>Loading...</h1>
+          : <ProductList products={products} />
+      }
+
     </ShopLayout>
   )
 }
