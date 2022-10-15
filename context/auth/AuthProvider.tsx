ac53ee119829ef0131c6ebfdb554077c7e349609
Sergio Cano <canosergio90@gmail.com>
2022-10-15 06:53:44 +0200

    keeping the user logged in by checking cookies
    Signed-off-by: Sergio Cano <canosergio90@gmail.com>

--- context/auth/AuthProvider.tsx
+++ context/auth/AuthProvider.tsx
@@ -1,6 +1,6 @@
 import axios from 'axios'
 import Cookies from 'js-cookie'
-import { FC, ReactNode, useReducer } from 'react'
+import { FC, ReactNode, useEffect, useReducer } from 'react'
 import { tesloApi } from '../../api'
 import { IUser } from '../../interfaces/user'
 import { AuthContext, authReducer } from './'
@@ -21,6 +21,22 @@ interface Props {
 
 export const AuthProvider: FC<Props> = ({ children }) => {
   const [state, dispatch] = useReducer(authReducer, AUTH_INITIAL_STATE)
+
+  useEffect(() => {
+    checkToken()
+  }, [])
+
+  const checkToken = async() => {
+    try {
+      const { data } = await tesloApi.get('/users/validate-token')
+      const { token, user } = data
+      Cookies.set('token', token)
+      dispatch({ type: '[Auth] - Login', payload: user })
+    } catch (error) {
+      Cookies.remove('token')
+    }
+  }
+  
 
   const loginUser = async (
     email: string,
