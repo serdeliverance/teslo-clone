d94e490c9fccd7b7c0760ff9cfb57888c98426f7
Sergio Cano <canosergio90@gmail.com>
2022-10-19 11:06:55 +0200

    av
    Signed-off-by: Sergio Cano <canosergio90@gmail.com>

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,12 +1,27 @@
 import NextAuth from "next-auth"
 import GithubProvider from "next-auth/providers/github"
+import Credentials from "next-auth/providers/credentials"
 
 export const authOptions = {
   providers: [
+    Credentials({
+      name: 'Custom Login',
+      credentials: {
+        email: { label: 'Email', type: 'email', placeholder: 'email@something.com'},
+        password: { label: 'Password', type: 'password', placeholder: 'password'},
+      },
+      async authorize(credentials) {
+        
+        return { email: 'pepe@gmail.com', role: 'admin'}
+      }
+    }),
     GithubProvider({
       clientId: process.env.GITHUB_ID || '',
       clientSecret: process.env.GITHUB_SECRET || '',
-    }),
+    })
   ],
+  callbacks: {
+
+  }
 }
 export default NextAuth(authOptions)
